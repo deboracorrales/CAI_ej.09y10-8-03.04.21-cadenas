141a18e6c9751584f040a68b7179a0f87b69dd3b
deboracorrales <deboramailen88@gmail.com>
2021-04-11 03:29:50 -0300

    Agregar archivos de proyecto.

---
+++ CAI_ej.09y10-8-03.04.21-cadenas/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_ej._09y10_8_03._04._21_cadenas
+/*Ej     
+9. Solicite un ingreso al usuario y lo presente en mayúsculas.
+10. Solicite un ingreso al usuario y lo presente en minúsculas.		*/
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto;
+
+            Console.WriteLine("Ingrese una palabra.");
+            texto = Console.ReadLine();
+
+            Console.WriteLine("Usted ingreso: " + texto.ToUpper());
+            Console.WriteLine("Usted ingreso: " + texto.ToLower());
+
+            Console.ReadKey();
+        }
+    }
+}
